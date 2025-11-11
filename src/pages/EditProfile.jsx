35858bd70badd26840c9e3c96607f37d1ac428c6
Aji Noto Sutrisno <ajisemail@example.com>
2025-11-11 08:49:39 +0700

    Test

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import { ArrowLeft, User, Upload, X } from "lucide-react";
+import api from "../../api/api";
 
 export default function EditProfile() {
   const [user, setUser] = useState({
