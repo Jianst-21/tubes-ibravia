fd011c68ee5a67dfa6871dc6f174a9442a867402
Aji Noto Sutrisno <ajisemail@example.com>
2025-11-14 00:24:02 +0700

    Footer Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import { HeroSection } from "../components/HomePage/HeroSection";
 import { TrustedCollaboration } from "../components/HomePage/TrustedCollaboration";
 import { WhyChooseUs } from "../components/HomePage/WhyChooseUs";
 import  {HouseCollections}  from "../components/HomePage/HouseCollections";
-import { Footer } from "../components/GlobalPage/Footer";
+
+import Footer from "../components/GlobalPage/Footer";
 
 export const Home = () => {
     return <div className=" min-h-screen overflow-x-hidden">
@@ -24,7 +25,8 @@ export const Home = () => {
            
             <HouseCollections />
         </main>
-        {Footer/}
+        {/*Footer*/}
+        <Footer />
 
 
     </div>;
