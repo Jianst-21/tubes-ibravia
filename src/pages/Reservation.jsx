25fe44d7c65f761a24ec1b631012b263f3111d07
Aji Noto Sutrisno <ajisemail@example.com>
2025-11-17 12:57:40 +0700

    Update reservation

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -3,27 +3,19 @@ import MyReservations from "../components/ReservationPage/MyReservations";
 import Footer from "../components/GlobalPage/Footer";
 
 export const Reservation = () => {
-    return <div className=" min-h-screen overflow-x-hidden">
+    return (
+        <div className="min-h-screen flex flex-col overflow-x-hidden">
 
-        {/* Theme Toggle */}
-        {/* <ThemeToggle /> */}
+            {/*Navbar*/}
+            <Navbar />
 
-        {/*Navbar*/}
-        <Navbar />
+            {/*Main Content*/}
+            <main className="flex-grow">
+                <MyReservations />
+            </main>
 
-
-        {/*Main Content*/}
-        <main>
-            <MyReservations />
-            {/* <HeroSection />
-            <TrustedCollaboration />
-            <WhyChooseUs />
-            <HouseCollections /> */}
-        </main>
-        {/*Footer*/}
-        <Footer />
-
-
-    </div>;
-
-}
+            {/*Footer*/}
+            <Footer />
+        </div>
+    );
+};
