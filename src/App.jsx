54793cc33febecac71e5f8b4ffbebc70207504c0
Ghilbranalf <ghilbranroyale@gmail.com>
2025-12-02 03:05:22 +0700

    ngerubah app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Login } from "./pages/Login";
+import OauthSuccess from "./pages/OauthSuccess";
 import { Properties } from "./pages/Properties";
 import { Reservation } from "./pages/Reservation";
 import { AboutUs } from "./pages/AboutUs";
 import { SignUp } from "./pages/SignUp";
 import { VerifyOTP } from "./pages/VerifyOTP";
-import { DetailProperties } from "./pages/DetailProperties";
+import { DetailProperties } from "./pages/detailProperties";
 import { Block } from "./components/PropertiesPage/Block";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { ResetPassword } from "./pages/ResetPassword";
 import EditProfile from "./pages/EditProfile";
-import OauthSuccess from "./pages/OauthSuccess";
 
 import AdminDashboard from "./pages/AdminDashboard";
 import ManageHouse from "./pages/ManageHouse";
@@ -20,11 +20,25 @@ import Notification from "./pages/Notification";
 import DataReport from "./pages/DataReport";
 import ManageReservation from "./pages/ManageReservation";
 import { AdminProtectedRoute, UserProtectedRoute } from "./lib/ProtectedRoute";
-// ProtectedRoute versi user
 
-// trigger redeploy
+import { useEffect } from "react";
 
 function App() {
+
+  // ============================
+  // 🚫 Auto-clear session admin
+  // ============================
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user?.role === "admin") {
+      console.log("Admin session cleared (prevent admin as user)");
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      localStorage.setItem("isLoggedIn", "false");
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -35,7 +49,7 @@ function App() {
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/VerifyOTP" element={<VerifyOTP />} />
         <Route path="/EditProfile" element={<EditProfile />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
         <Route path="/ResetPassword" element={<ResetPassword />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/oauth-success" element={<OauthSuccess />} />
