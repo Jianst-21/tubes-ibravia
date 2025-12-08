ceedd7a37fed52f47458e3e99b217cd0a4df61c6
Ghilbranalf <ghilbranroyale@gmail.com>
2025-12-08 22:36:06 +0700

    header

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -126,7 +126,7 @@ const AdminDashboard = () => {
         <div className="px-6 sm:px-8 py-6 flex-1 flex flex-col">
           <div className="max-w-[1100px] mx-auto w-full">
             {/* TITLE */}
-            <h1 className="text-[48px] font-bold text-gray-900 -mt-8 mb-8">
+            <h1 className="text-[48px] font-bold text-gray-900 -mt-4 mb-8">
               Dashboard {residenceName}
             </h1>
 
